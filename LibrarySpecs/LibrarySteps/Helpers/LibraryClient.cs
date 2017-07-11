ab7edab96bcd13af3a6fde16afcb88b7aa889bc0
Jeff Langr <jeff@langrsoft.com>
2017-07-10 22:18:58 -0600

    factored

---
+++ LibrarySpecs/LibrarySteps/Helpers/LibraryClient.cs
@@ -0,0 +1,56 @@
+﻿using Library.Controllers;
+using Library.Models;
+using Library.Models.Repositories;
+using Library.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySpecs.LibrarySteps.Helpers
+{
+    public class LibraryClient
+    {
+        private InMemoryRepository<Branch> branchRepo;
+        private InMemoryRepository<Holding> holdingRepo;
+        private InMemoryRepository<Patron> patronRepo;
+        private CheckOutController checkoutController;
+
+        public void Initialize()
+        {
+            holdingRepo = new InMemoryRepository<Holding>();
+            branchRepo = new InMemoryRepository<Branch>();
+            patronRepo = new InMemoryRepository<Patron>();
+
+            checkoutController = new CheckOutController(branchRepo, holdingRepo, patronRepo);
+        }
+
+        internal int Create(Branch branch)
+        {
+            return branchRepo.Create(branch);
+        }
+
+        internal int Create(Patron patron)
+        {
+            return patronRepo.Create(patron);
+        }
+
+        internal int Create(Holding holding)
+        {
+            return holdingRepo.Create(holding);
+        }
+
+        internal void Checkout(DateTime date, string barcode, int patronId)
+        {
+            TimeService.NextTime = date;
+            checkoutController.Index(
+                new CheckOutViewModel { Barcode = barcode, PatronId = patronId });
+        }
+
+        internal Holding GetHoldingByID(int id)
+        {
+            return holdingRepo.GetByID(id);
+        }
+    }
+}
--- LibrarySpecs/LibrarySteps/PatronBorrowing/FinesSteps.cs
+++ LibrarySpecs/LibrarySteps/PatronBorrowing/FinesSteps.cs
@@ -5,31 +5,26 @@ using Library.Models.Repositories;
 using Library.Controllers;
 using Library.Models;
 using Library.Util;
+using LibrarySpecs.LibrarySteps.Helpers;
 
 namespace LibrarySpecs.Properties
 {
     [Binding]
     public class FinesSteps
     {
-        private InMemoryRepository<Holding> holdingRepo;
         private int someValidPatronId;
         private int someValidBranchId;
-        private CheckOutController controller;
         private Holding aCheckedInHolding;
         private int holdingId;
-        private CheckOutViewModel checkout;
+
+        private LibraryClient client = new LibraryClient();
 
         [Given(@"a library system with one book")]
         public void GivenALibrarySystemWithOneBook()
         {
-            holdingRepo = new InMemoryRepository<Holding>();
-            var branchRepo = new InMemoryRepository<Branch>();
-            someValidBranchId = branchRepo.Create(new Branch() { Name = "b" });
-
-            var patronRepo = new InMemoryRepository<Patron>();
-            someValidPatronId = patronRepo.Create(new Patron { Name = "x" });
-
-            controller = new CheckOutController(branchRepo, holdingRepo, patronRepo);
+            client.Initialize();
+            someValidBranchId = client.Create(new Branch() { Name = "b" });
+            someValidPatronId = client.Create(new Patron { Name = "x" });
             CreateCheckedInHolding();
         }
 
@@ -37,21 +32,19 @@ namespace LibrarySpecs.Properties
         {
             aCheckedInHolding = new Holding { Classification = "ABC", CopyNumber = 1 };
             aCheckedInHolding.CheckIn(DateTime.Now, someValidBranchId);
-            holdingId = holdingRepo.Create(aCheckedInHolding);
+            holdingId = client.Create(aCheckedInHolding);
         }
 
         [When(@"a patron checks out the book on (.*)")]
         public void WhenAPatronChecksOutTheBookOn(DateTime date)
         {
-            checkout = new CheckOutViewModel { Barcode = aCheckedInHolding.Barcode, PatronId = someValidPatronId };
-            TimeService.NextTime = date;
-            controller.Index(checkout);
+            client.Checkout(date, aCheckedInHolding.Barcode, someValidPatronId);
         }
         
         [Then(@"the due date is (.*)")]
         public void ThenTheDueDateIs(DateTime date)
         {
-            var retrievedHolding = holdingRepo.GetByID(holdingId);
+            var retrievedHolding = client.GetHoldingByID(holdingId); 
             Assert.That(retrievedHolding.DueDate, Is.EqualTo(date));
         }
     }
